5fc4711a89b96742e248fe69ac571da977c9286b
Ryan Parlin <parlinr@ad.nmc.edu>
2018-02-22 14:58:28 -0500

    converted project to .NET framework, on p.6 of lab packet

---
+++ TopicalInformationApp/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopicalInformationApp.Models
+{
+    public class Book
+    {
+        public string title { get; set; }
+        public List<string> authors { get; set; }
+        public string publicationYear { get; set; }
+        public string summary { get; set; }
+
+    }
+}
---
+++ TopicalInformationApp/Models/Books.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace TopicalInformationApp.Models
+{
+    [XmlRoot("books")]
+    public class Books
+    {
+        [XmlElement("book")]
+        public List<Book> books;
+    }
+}
